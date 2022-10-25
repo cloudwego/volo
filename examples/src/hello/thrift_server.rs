daa18caa54abd61afa2c15d10a1b2d90585dfb0e
Pure White <wudi@purewhite.io>
2022-10-25 16:52:07 +0800

    doc: add examples

---
+++ examples/src/hello/thrift_server.rs
@@ -0,0 +1,29 @@
+#![feature(type_alias_impl_trait)]
+
+use std::net::SocketAddr;
+
+pub struct S;
+
+#[volo::async_trait]
+impl volo_gen::thrift_gen::hello::HelloService for S {
+    async fn hello(
+        &self,
+        req: volo_gen::thrift_gen::hello::HelloRequest,
+    ) -> Result<volo_gen::thrift_gen::hello::HelloResponse, ::volo_thrift::AnyhowError> {
+        let resp = volo_gen::thrift_gen::hello::HelloResponse {
+            message: format!("Hello, {}!", req.name),
+        };
+        Ok(resp)
+    }
+}
+
+#[volo::main]
+async fn main() {
+    let addr: SocketAddr = "[::]:8081".parse().unwrap();
+    let addr = volo::net::Address::from(addr);
+
+    volo_gen::thrift_gen::hello::HelloServiceServer::new(S)
+        .run(addr)
+        .await
+        .unwrap();
+}
