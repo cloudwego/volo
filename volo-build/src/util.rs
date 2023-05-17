dd69c1fdda4d05a00adc3a32c422e40102eba708
yifei <lyfmagic99@gmail.com>
2023-05-17 17:49:14 +0800

    fix: download dir

--- volo-build/src/util.rs
+++ volo-build/src/util.rs
@@ -69,7 +69,7 @@ pub struct LocalIdl {
     pub touch: Vec<String>,
 }
 
-pub fn get_or_download_idl(idl: Idl) -> anyhow::Result<LocalIdl> {
+pub fn get_or_download_idl(idl: Idl, target_dir: impl AsRef<Path>) -> anyhow::Result<LocalIdl> {
     let (path, includes) = if let Source::Git(GitSource {
         ref repo, ref lock, ..
     }) = idl.source
@@ -80,7 +80,10 @@ pub fn get_or_download_idl(idl: Idl) -> anyhow::Result<LocalIdl> {
                 repo
             )
         })?;
-        let dir = DEFAULT_DIR.join(get_git_path(repo.as_str())?).join(lock);
+        let dir = target_dir
+            .as_ref()
+            .join(get_git_path(repo.as_str())?)
+            .join(lock);
         let task = Task::new(
             vec![idl.path.to_string_lossy().to_string()],
             dir.clone(),
