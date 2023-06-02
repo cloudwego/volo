56d1472164339a45832138c6d8d03e7a9cb6ebae
ethe <gwo.tzuhsing@gmail.com>
2023-06-02 14:40:15 +0800

    move tracing start after decode

--- volo-thrift/src/tracing.rs
+++ volo-thrift/src/tracing.rs
@@ -3,32 +3,20 @@ use tracing::Span;
 use crate::context::ServerContext;
 
 pub trait SpanProvider: 'static + Send + Sync + Clone {
-    #[inline]
-    fn on_serve(&self) -> Span {
-        Span::none()
-    }
-
-    #[inline]
-    fn on_decode(&self) -> Span {
-        Span::none()
-    }
-
-    #[inline]
-    fn on_encode(&self) -> Span {
-        Span::none()
-    }
-
-    #[inline]
-    fn leave_decode(&self, context: &ServerContext) {
+    fn on_serve(&self, context: &ServerContext) -> Span {
         let _ = context;
+        Span::none()
     }
 
-    #[inline]
+    fn on_encode(&self, context: &ServerContext) -> Span {
+        let _ = context;
+        Span::none()
+    }
+
     fn leave_encode(&self, context: &ServerContext) {
         let _ = context;
     }
 
-    #[inline]
     fn leave_serve(&self, context: &ServerContext) {
         let _ = context;
     }
