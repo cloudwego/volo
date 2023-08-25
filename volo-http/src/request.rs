3eb0b48122858800921974888ec61877278763fe
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2023-08-25 14:54:24 +0800

    init

---
+++ volo-http/src/request.rs
@@ -0,0 +1,93 @@
+use bytes::Bytes;
+use futures_util::Future;
+use http::{header, HeaderMap, Response, StatusCode};
+use http_body_util::BodyExt;
+use hyper::body::Incoming;
+use serde::de::DeserializeOwned;
+
+use crate::{response::RespBody, HttpContext};
+
+pub trait FromRequest: Sized {
+    type FromFut<'cx>: Future<Output = Result<Self, Response<RespBody>>> + Send + 'cx
+    where
+        Self: 'cx;
+
+    fn from(cx: &HttpContext, body: Incoming) -> Self::FromFut<'_>;
+}
+
+impl FromRequest for Incoming {
+    type FromFut<'cx> = impl Future<Output = Result<Self, Response<RespBody>>> + Send + 'cx
+    where
+        Self: 'cx;
+
+    fn from(_cx: &HttpContext, body: Incoming) -> Self::FromFut<'_> {
+        async { Ok(body) }
+    }
+}
+
+pub struct Json<T>(pub T);
+
+impl<T: DeserializeOwned> FromRequest for Json<T> {
+    type FromFut<'cx> = impl Future<Output = Result<Self, Response<RespBody>>> + Send + 'cx
+    where
+        Self: 'cx;
+
+    fn from(cx: &HttpContext, body: Incoming) -> Self::FromFut<'_> {
+        async move {
+            if !json_content_type(&cx.headers) {
+                return Err(Response::builder()
+                    .status(StatusCode::UNSUPPORTED_MEDIA_TYPE)
+                    .body(Bytes::new().into())
+                    .unwrap());
+            }
+
+            match body.collect().await {
+                Ok(body) => {
+                    let body = body.to_bytes();
+                    match serde_json::from_slice::<T>(body.as_ref()) {
+                        Ok(t) => Ok(Self(t)),
+                        Err(e) => {
+                            tracing::warn!("json serialization error {e}");
+                            Err(Response::builder()
+                                .status(StatusCode::BAD_REQUEST)
+                                .body(Bytes::new().into())
+                                .unwrap())
+                        }
+                    }
+                }
+                Err(e) => {
+                    tracing::warn!("collect body error: {e}");
+                    Err(Response::builder()
+                        .status(StatusCode::BAD_REQUEST)
+                        .body(Bytes::new().into())
+                        .unwrap())
+                }
+            }
+        }
+    }
+}
+
+fn json_content_type(headers: &HeaderMap) -> bool {
+    let content_type = if let Some(content_type) = headers.get(header::CONTENT_TYPE) {
+        content_type
+    } else {
+        return false;
+    };
+
+    let content_type = if let Ok(content_type) = content_type.to_str() {
+        content_type
+    } else {
+        return false;
+    };
+
+    let mime = if let Ok(mime) = content_type.parse::<mime::Mime>() {
+        mime
+    } else {
+        return false;
+    };
+
+    let is_json_content_type = mime.type_() == "application"
+        && (mime.subtype() == "json" || mime.suffix().map_or(false, |name| name == "json"));
+
+    is_json_content_type
+}
---
+++ volo-http/src/route.rs
@@ -0,0 +1,287 @@
+use std::{future::Future, net::SocketAddr};
+
+use http::{Method, Response, StatusCode};
+use http_body_util::Full;
+use hyper::{
+    body::{Bytes, Incoming},
+    server::conn::http1,
+};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+use crate::{
+    dispatch::DispatchService, request::FromRequest, response::RespBody, DynError, HttpContext,
+    HttpContextInner, MotoreService,
+};
+
+pub type DynService = motore::BoxCloneService<HttpContext, Incoming, Response<RespBody>, DynError>;
+
+#[derive(Clone)]
+pub struct Router {
+    inner: matchit::Router<DynService>,
+}
+
+impl Router {
+    pub fn build() -> RouterBuilder {
+        Default::default()
+    }
+}
+
+impl motore::Service<(), (HttpContextInner, Incoming)> for Router {
+    type Response = Response<RespBody>;
+
+    type Error = DynError;
+
+    type Future<'cx> = impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'cx
+    where
+        HttpContextInner: 'cx,
+        Self: 'cx;
+
+    fn call<'cx, 's>(
+        &'s self,
+        _cx: &'cx mut (),
+        cxreq: (HttpContextInner, Incoming),
+    ) -> Self::Future<'cx>
+    where
+        's: 'cx,
+    {
+        async move {
+            let (cx, req) = cxreq;
+            if let Ok(matched) = self.inner.at(cx.uri.path()) {
+                let mut context = HttpContext {
+                    peer: cx.peer,
+                    method: cx.method,
+                    uri: cx.uri.clone(),
+                    version: cx.version,
+                    headers: cx.headers,
+                    extensions: cx.extensions,
+                    params: matched.params.into(),
+                };
+                matched.value.call(&mut context, req).await
+            } else {
+                Ok(Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Full::new(Bytes::new()).into())
+                    .unwrap())
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct RouterBuilder {
+    routes: matchit::Router<DynService>,
+}
+
+impl RouterBuilder {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn route<R, S>(mut self, uri: R, route: S) -> Self
+    where
+        R: Into<String>,
+        S: motore::Service<HttpContext, Incoming, Response = Response<RespBody>, Error = DynError>
+            + Send
+            + Sync
+            + Clone
+            + 'static,
+    {
+        if let Err(e) = self.routes.insert(uri, motore::BoxCloneService::new(route)) {
+            panic!("routing error: {e}");
+        }
+        self
+    }
+
+    pub async fn serve(self, addr: SocketAddr) -> Result<(), DynError> {
+        let listener = TcpListener::bind(addr).await?;
+        let router = Router { inner: self.routes };
+
+        loop {
+            let s = router.clone();
+            let (stream, peer) = listener.accept().await?;
+
+            let io = TokioIo::new(stream);
+
+            tokio::task::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(io, MotoreService { peer, inner: s })
+                    .await
+                {
+                    tracing::warn!("error serving connection: {:?}", err);
+                }
+            });
+        }
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct Route {
+    options: Option<DynService>,
+    get: Option<DynService>,
+    post: Option<DynService>,
+    put: Option<DynService>,
+    delete: Option<DynService>,
+    head: Option<DynService>,
+    trace: Option<DynService>,
+    connect: Option<DynService>,
+    patch: Option<DynService>,
+}
+
+impl Route {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn builder() -> RouteBuilder {
+        RouteBuilder { route: Self::new() }
+    }
+}
+
+impl motore::Service<HttpContext, Incoming> for Route {
+    type Response = Response<RespBody>;
+
+    type Error = DynError;
+
+    type Future<'cx> = impl Future<Output = Result<Self::Response, Self::Error>> + Send + 'cx
+    where
+        HttpContext: 'cx,
+        Self: 'cx;
+
+    fn call<'cx, 's>(&'s self, cx: &'cx mut HttpContext, req: Incoming) -> Self::Future<'cx>
+    where
+        's: 'cx,
+    {
+        async move {
+            match cx.method {
+                Method::GET => {
+                    if let Some(service) = &self.get {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::POST => {
+                    if let Some(service) = &self.post {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::PUT => {
+                    if let Some(service) = &self.put {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::DELETE => {
+                    if let Some(service) = &self.delete {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::HEAD => {
+                    if let Some(service) = &self.head {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::OPTIONS => {
+                    if let Some(service) = &self.options {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::CONNECT => {
+                    if let Some(service) = &self.connect {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::PATCH => {
+                    if let Some(service) = &self.patch {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                Method::TRACE => {
+                    if let Some(service) = &self.trace {
+                        service.call(cx, req).await
+                    } else {
+                        Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body("".into())
+                            .unwrap())
+                    }
+                }
+                _ => Ok(Response::builder()
+                    .status(StatusCode::METHOD_NOT_ALLOWED)
+                    .body("".into())
+                    .unwrap()),
+            }
+        }
+    }
+}
+
+macro_rules! impl_method_register {
+    ($( $method:ident ),*) => {
+        $(
+        pub fn $method<S, IB, OB>(mut self, handler: S) -> Self
+        where
+            S: motore::Service<HttpContext, IB, Response = Response<OB>>
+                + Send
+                + Sync
+                + Clone
+                + 'static,
+            S::Error: std::error::Error + Send + Sync,
+            OB: Into<RespBody> + 'static,
+            IB: FromRequest + Send + 'static,
+        {
+            self.route.$method = Some(motore::BoxCloneService::new(DispatchService::new(handler)));
+            self
+        }
+        )+
+    };
+}
+
+pub struct RouteBuilder {
+    route: Route,
+}
+
+impl RouteBuilder {
+    impl_method_register!(options, get, post, put, delete, head, trace, connect, patch);
+
+    pub fn build(self) -> Route {
+        self.route
+    }
+}
