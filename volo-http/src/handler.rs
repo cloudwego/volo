d4736d96d26e1c1190f4f0e9c869a7f735ddce43
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2023-09-04 18:55:18 +0800

    update

--- volo-http/src/handler.rs
+++ volo-http/src/handler.rs
@@ -70,22 +70,22 @@ impl_handler!([T1, T2, T3], T4);
 impl_handler!([T1, T2, T3, T4], T5);
 impl_handler!([T1, T2, T3, T4, T5], T6);
 impl_handler!([T1, T2, T3, T4, T5, T6], T7);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7], T8);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8], T9);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9], T10);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10], T11);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11], T12);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12], T13);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7], T8);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7, T8], T9);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7, T8, T9], T10);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
+impl_handler!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
 impl_handler!(
-    [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13],
+    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13],
     T14
 );
 impl_handler!(
-    [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14],
+    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14],
     T15
 );
 impl_handler!(
-    [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14, T15],
+    [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15],
     T16
 );
 
