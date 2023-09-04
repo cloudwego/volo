061e58154a4251d830805c89677d1390e9e64c37
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2023-09-04 17:55:55 +0800

    use HRTB

--- volo-http/src/handler.rs
+++ volo-http/src/handler.rs
@@ -20,26 +20,30 @@ where
         }
     }
 }
-pub trait Handler<'r, T> {
-    type Future: Future<Output = Response<RespBody>> + Send + 'r;
-    fn call(self, context: &'r mut HttpContext, req: Incoming) -> Self::Future;
+pub trait Handler<T> {
+    type Future<'r>: Future<Output = Response<RespBody>> + Send + 'r
+    where
+        Self: 'r;
+    fn call(self, context: &mut HttpContext, req: Incoming) -> Self::Future<'_>;
 }
 
 macro_rules! impl_handler {
     (
-        [$($ty:ident),*],
+        [$($ty:ident),*], /* $last:ident */
     ) => {
         #[allow(non_snake_case, unused_mut, unused_variables)]
-        impl<'r, F, Fut, $($ty,)* Res> Handler<'r, ($($ty,)*)> for F
+        impl<F, Fut, $($ty,)* /* $last, */ Res> Handler<($($ty,)* /* $last, */)> for F
         where
-            F: FnOnce($($ty,)*) -> Fut + Clone + Send + 'r,
+            F: FnOnce($($ty,)*) -> Fut + Clone + Send,
             Fut: Future<Output = Res> + Send,
-            $( $ty: FromContext + Send + 'r, )*
+            $( for<'r> $ty: FromContext + Send + 'r, )*
+            // for<'r> $last: FromRequest + Send + 'r,
             Res: IntoResponse,
         {
-            type Future = impl Future<Output = Response<RespBody>> + Send + 'r;
+            type Future<'r> = impl Future<Output=Response<RespBody>> + Send + 'r
+                where Self: 'r;
 
-            fn call(self, context: &'r mut HttpContext, _req: Incoming) -> Self::Future {
+            fn call(self, context: &mut HttpContext, req: Incoming) -> Self::Future<'_> {
                 async move {
                     $(
                         let $ty = match $ty::from_context(context).await {
@@ -47,6 +51,10 @@ macro_rules! impl_handler {
                             Err(rejection) => return rejection.into_response(),
                         };
                     )*
+                    // let $last = match $last::from(context, req).await {
+                    //     Ok(value) => value,
+                    //     Err(rejection) => return rejection,
+                    // };
                     self($($ty,)*).await.into_response()
                 }
             }
@@ -57,19 +65,31 @@ macro_rules! impl_handler {
 impl_handler!([],);
 impl_handler!([T1],);
 impl_handler!([T1, T2],);
-impl_handler!([T1, T2, T3],);
-impl_handler!([T1, T2, T3, T4],);
-impl_handler!([T1, T2, T3, T4, T5],);
-impl_handler!([T1, T2, T3, T4, T5, T6],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14],);
-impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14, T15],);
+// impl_handler!([], T1);
+// impl_handler!([T1], T2);
+// impl_handler!([T1, T2], T3);
+// impl_handler!([T1, T2, T3], T4);
+// impl_handler!([T1, T2, T3, T4], T5);
+// impl_handler!([T1, T2, T3, T4, T5], T6);
+// impl_handler!([T1, T2, T3, T4, T5, T6], T7);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7], T8);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8], T9);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9], T10);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10], T11);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11], T12);
+// impl_handler!([T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12], T13);
+// impl_handler!(
+//     [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13],
+//     T14
+// );
+// impl_handler!(
+//     [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14],
+//     T15
+// );
+// impl_handler!(
+//     [T1, T2, T3, T4, T5, T6, Y7, T8, T9, T10, T11, T12, T13, T14, T15],
+//     T16
+// );
 
 pub struct HandlerService<H, T> {
     h: H,
@@ -87,7 +107,7 @@ impl<H, T> HandlerService<H, T> {
 
 impl<H, T> motore::Service<HttpContext, Incoming> for HandlerService<H, T>
 where
-    H: for<'r> Handler<'r, T> + Clone + Send + Sync,
+    for<'r> H: Handler<T> + Clone + Send + Sync + 'r,
 {
     type Response = Response<RespBody>;
     type Error = http::Error;
