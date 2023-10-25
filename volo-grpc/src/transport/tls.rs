06d3a865ca6873d57493f1808d929d27fc6d23b5
minghuaw <michael.wu1107@gmail.com>
2023-10-24 23:37:51 -0700

    changed cfg attr to macro

--- volo-grpc/src/transport/tls.rs
+++ volo-grpc/src/transport/tls.rs
@@ -1,53 +1,50 @@
-/// TLS configuration for a server.
-#[doc(cfg(any(feature = "rustls", feature = "native-tls")))]
-#[cfg(any(feature = "rustls", feature = "native-tls"))]
-#[derive(Clone)]
-pub struct ServerTlsConfig {
-    pub acceptor: TlsAcceptor,
+cfg_rustls_or_native_tls! {
+    /// TLS configuration for a server.
+    #[derive(Clone)]
+    pub struct ServerTlsConfig {
+        pub acceptor: TlsAcceptor,
+    }
+    
+    /// A wrapper around [`tokio_rustls::TlsAcceptor`] and [`tokio_native_tls::TlsAcceptor`].
+    #[derive(Clone)]
+    pub enum TlsAcceptor {
+        /// `tokio_rustls::TlsAcceptor` internally uses `Arc`
+        #[doc(cfg(feature = "rustls"))]
+        #[cfg(feature = "rustls")]
+        Rustls(tokio_rustls::TlsAcceptor),
+    
+        /// This takes an `Arc` because it does not internally use `Arc`
+        #[doc(cfg(feature = "native-tls"))]
+        #[cfg(feature = "native-tls")]
+        NativeTls(std::sync::Arc<tokio_native_tls::TlsAcceptor>),
+    }
 }
 
-/// A wrapper around [`tokio_rustls::TlsAcceptor`] and [`tokio_native_tls::TlsAcceptor`].
-#[doc(cfg(any(feature = "rustls", feature = "native-tls")))]
-#[cfg(any(feature = "rustls", feature = "native-tls"))]
-#[derive(Clone)]
-pub enum TlsAcceptor {
-    /// `tokio_rustls::TlsAcceptor` internally uses `Arc`
-    #[doc(cfg(feature = "rustls"))]
-    #[cfg(feature = "rustls")]
-    Rustls(tokio_rustls::TlsAcceptor),
-
-    /// This takes an `Arc` because it does not internally use `Arc`
-    #[doc(cfg(feature = "native-tls"))]
-    #[cfg(feature = "native-tls")]
-    NativeTls(std::sync::Arc<tokio_native_tls::TlsAcceptor>),
-}
-
-#[doc(cfg(feature = "rustls"))]
-#[cfg(feature = "rustls")]
-impl From<tokio_rustls::TlsAcceptor> for ServerTlsConfig {
-    fn from(value: tokio_rustls::TlsAcceptor) -> Self {
-        Self {
-            acceptor: TlsAcceptor::Rustls(value),
+cfg_rustls! {
+    impl From<tokio_rustls::TlsAcceptor> for ServerTlsConfig {
+        fn from(value: tokio_rustls::TlsAcceptor) -> Self {
+            Self {
+                acceptor: TlsAcceptor::Rustls(value),
+            }
         }
     }
 }
 
-#[doc(cfg(feature = "native-tls"))]
-#[cfg(feature = "native-tls")]
-impl From<std::sync::Arc<tokio_native_tls::TlsAcceptor>> for ServerTlsConfig {
-    fn from(value: std::sync::Arc<tokio_native_tls::TlsAcceptor>) -> Self {
-        Self {
-            acceptor: TlsAcceptor::NativeTls(value),
+cfg_native_tls! {
+    impl From<std::sync::Arc<tokio_native_tls::TlsAcceptor>> for ServerTlsConfig {
+        fn from(value: std::sync::Arc<tokio_native_tls::TlsAcceptor>) -> Self {
+            Self {
+                acceptor: TlsAcceptor::NativeTls(value),
+            }
+        }
+    }
+    
+    impl From<tokio_native_tls::TlsAcceptor> for ServerTlsConfig {
+        fn from(value: tokio_native_tls::TlsAcceptor) -> Self {
+            Self {
+                acceptor: TlsAcceptor::NativeTls(std::sync::Arc::new(value)),
+            }
         }
     }
 }
 
-#[doc(cfg(feature = "native-tls"))]
-#[cfg(feature = "native-tls")]
-impl From<tokio_native_tls::TlsAcceptor> for ServerTlsConfig {
-    fn from(value: tokio_native_tls::TlsAcceptor) -> Self {
-        Self {
-            acceptor: TlsAcceptor::NativeTls(std::sync::Arc::new(value)),
-        }
-    }
-}
