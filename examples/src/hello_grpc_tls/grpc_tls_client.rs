9fb6683787ec3fbafca05e48bbbb6e7e3c4385f6
minghuaw <michael.wu1107@gmail.com>
2023-10-25 02:32:58 -0700

    added example for tls

---
+++ examples/src/hello_grpc_tls/grpc_tls_client.rs
@@ -0,0 +1,52 @@
+//! Run with `cargo run --example hello-tls-grpc-client --features tls`
+
+#![feature(impl_trait_in_assoc_type)]
+
+use std::{net::SocketAddr, path::Path, sync::Arc};
+
+use pilota::FastStr;
+use rustls::{Certificate, RootCertStore};
+use rustls_pemfile::certs;
+use volo::net::dial::ClientTlsConfig;
+
+fn load_certs(path: impl AsRef<Path>) -> std::io::Result<Vec<Certificate>> {
+    certs(&mut std::io::BufReader::new(std::fs::File::open(path)?))
+        .map(|v| v.into_iter().map(Certificate).collect())
+        .map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid cert").into())
+}
+
+#[volo::main]
+async fn main() {
+    // The key and certificate are copied from
+    // https://github.com/hyperium/tonic/tree/master/examples/data/tls
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
+    let root_cert = load_certs(data_dir.join("tls/ca.pem")).unwrap();
+    let mut root_certs = RootCertStore::empty();
+    root_certs.add(&root_cert[0]).unwrap();
+
+    let client_config = rustls::ClientConfig::builder()
+        .with_safe_default_cipher_suites()
+        .with_safe_default_kx_groups()
+        .with_safe_default_protocol_versions()
+        .unwrap()
+        .with_root_certificates(root_certs)
+        .with_no_client_auth();
+    let client_config = Arc::new(client_config);
+    let connector = tokio_rustls::TlsConnector::from(client_config);
+    let tls_config = ClientTlsConfig::new("example.com", connector);
+
+    let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let client = volo_gen::proto_gen::hello::GreeterClientBuilder::new("hello")
+        .tls_config(tls_config)
+        .address(addr)
+        .build();
+
+    let req = volo_gen::proto_gen::hello::HelloRequest {
+        name: FastStr::from_static_str("Volo"),
+    };
+    let resp = client.say_hello(req).await;
+    match resp {
+        Ok(info) => println!("{info:?}"),
+        Err(e) => eprintln!("{e:?}"),
+    }
+}
---
+++ examples/src/hello_grpc_tls/grpc_tls_server.rs
@@ -0,0 +1,73 @@
+//! Run with `cargo run --example hello-tls-grpc-server --features tls`
+
+#![feature(impl_trait_in_assoc_type)]
+
+use std::{net::SocketAddr, path::Path, sync::Arc};
+
+use rustls::{Certificate, PrivateKey};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use volo_grpc::{
+    server::{Server, ServiceBuilder},
+    transport::ServerTlsConfig,
+};
+
+pub struct S;
+
+#[volo::async_trait]
+impl volo_gen::proto_gen::hello::Greeter for S {
+    async fn say_hello(
+        &self,
+        req: volo_grpc::Request<volo_gen::proto_gen::hello::HelloRequest>,
+    ) -> Result<volo_grpc::Response<volo_gen::proto_gen::hello::HelloReply>, volo_grpc::Status>
+    {
+        let resp = volo_gen::proto_gen::hello::HelloReply {
+            message: format!("Hello, {}!", req.get_ref().name).into(),
+        };
+        Ok(volo_grpc::Response::new(resp))
+    }
+}
+
+fn load_certs(path: impl AsRef<Path>) -> std::io::Result<Vec<Certificate>> {
+    certs(&mut std::io::BufReader::new(std::fs::File::open(path)?))
+        .map(|v| v.into_iter().map(Certificate).collect())
+        .map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid cert").into())
+}
+
+fn load_keys(path: impl AsRef<Path>) -> std::io::Result<Vec<PrivateKey>> {
+    pkcs8_private_keys(&mut std::io::BufReader::new(std::fs::File::open(path)?))
+        .map(|v| v.into_iter().map(PrivateKey).collect())
+        .map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid key").into())
+}
+
+#[volo::main]
+async fn main() {
+    // TLS configuration 
+    // 
+    // The key and certificate are copied from
+    // https://github.com/hyperium/tonic/tree/master/examples/data/tls
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
+    let certs = load_certs(data_dir.join("tls/server.pem")).unwrap();
+    let private_key = load_keys(data_dir.join("tls/server.key")).unwrap()[0].clone();
+
+    let mut server_config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, private_key)
+        .expect("bad certificate/key");
+    server_config.alpn_protocols = vec![b"h2".to_vec()];
+
+    let server_config = Arc::new(server_config);
+    let acceptor = tokio_rustls::TlsAcceptor::from(server_config);
+    let tls_config = ServerTlsConfig::from(acceptor);
+
+    // Server address
+    let addr: SocketAddr = "[::]:8080".parse().unwrap();
+    let addr = volo::net::Address::from(addr);
+
+    Server::new()
+        .tls_config(tls_config)
+        .add_service(ServiceBuilder::new(volo_gen::proto_gen::hello::GreeterServer::new(S)).build())
+        .run(addr)
+        .await
+        .unwrap();
+}
