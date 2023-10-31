807a58c7013758e9c0a656ff81e7a7884175a0fa
Millione <isjieliu@163.com>
2023-10-31 18:54:53 +0800

    fix: shutdown_hook not send

--- volo-thrift/src/server.rs
+++ volo-thrift/src/server.rs
@@ -43,7 +43,7 @@ pub struct Server<S, L, Req, MkC, SP> {
     #[cfg(feature = "multiplex")]
     multiplex: bool,
     span_provider: SP,
-    shutdown_hooks: Vec<Box<dyn FnOnce() -> BoxFuture<'static, ()>>>,
+    shutdown_hooks: Vec<Box<dyn FnOnce() -> BoxFuture<'static, ()> + Send>>,
     _marker: PhantomData<Req>,
 }
 
@@ -81,7 +81,7 @@ impl<S, L, Req, MkC, SP> Server<S, L, Req, MkC, SP> {
     /// in reverse order of registration.
     pub fn register_shutdown_hook(
         mut self,
-        hook: impl FnOnce() -> BoxFuture<'static, ()> + 'static,
+        hook: impl FnOnce() -> BoxFuture<'static, ()> + 'static + Send,
     ) -> Self {
         self.shutdown_hooks.push(Box::new(hook));
         self
