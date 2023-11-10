e536be604e12d9dc30667f899e45e41b9fa128a8
Yu Li <liyu.yukiteru@bytedance.com>
2023-11-10 18:41:30 +0800

    feat(volo-http): introduce route id
    The `matchit::Router` cannot be converted to `Iterator`, so using
    `matchit::Router<DynService>` is not convenient enough.
    
    To solve the problem, we refer to the implementation of `axum` and
    introduce a `RouteId` as a bridge, the `matchit::Router` only handles
    some IDs and each ID corresponds to a `DynService`.
    
    Signed-off-by: Yu Li <liyu.yukiteru@bytedance.com>

--- volo-http/src/route.rs
+++ volo-http/src/route.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use http::{Method, Response, StatusCode};
 use http_body_util::Full;
 use hyper::body::{Bytes, Incoming};
@@ -7,11 +9,33 @@ use crate::{
     dispatch::DispatchService, request::FromRequest, response::RespBody, DynError, HttpContext,
 };
 
+// The `matchit::Router` cannot be converted to `Iterator`, so using `matchit::Router<DynService>`
+// is not convenient enough.
+//
+// To solve the problem, we refer to the implementation of `axum` and introduce a `RouteId` as a
+// bridge, the `matchit::Router` only handles some IDs and each ID corresponds to a `DynService`.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub(crate) struct RouteId(u32);
+
+impl RouteId {
+    fn next() -> Self {
+        use std::sync::atomic::{AtomicU32, Ordering};
+        // `AtomicU64` isn't supported on all platforms
+        static ID: AtomicU32 = AtomicU32::new(0);
+        let id = ID.fetch_add(1, Ordering::Relaxed);
+        if id == u32::MAX {
+            panic!("Over `u32::MAX` routes created. If you need this, please file an issue.");
+        }
+        Self(id)
+    }
+}
+
 pub type DynService = motore::BoxCloneService<HttpContext, Incoming, Response<RespBody>, DynError>;
 
 #[derive(Clone, Default)]
 pub struct Router {
-    inner: matchit::Router<DynService>,
+    matcher: matchit::Router<RouteId>,
+    routes: HashMap<RouteId, DynService>,
 }
 
 impl Service<HttpContext, Incoming> for Router {
@@ -24,15 +48,17 @@ impl Service<HttpContext, Incoming> for Router {
         cx: &'cx mut HttpContext,
         req: Incoming,
     ) -> Result<Self::Response, Self::Error> {
-        if let Ok(matched) = self.inner.at(cx.uri.path()) {
-            cx.params = matched.params.into();
-            matched.value.call(cx, req).await
-        } else {
-            Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Full::new(Bytes::new()).into())
-                .unwrap())
+        if let Ok(matched) = self.matcher.at(cx.uri.path()) {
+            if let Some(srv) = self.routes.get(matched.value) {
+                cx.params = matched.params.into();
+                return srv.call(cx, req).await;
+            }
         }
+
+        Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Full::new(Bytes::new()).into())
+            .unwrap())
     }
 }
 
@@ -50,9 +76,13 @@ impl Router {
             + Clone
             + 'static,
     {
-        if let Err(e) = self.inner.insert(uri, motore::BoxCloneService::new(route)) {
-            panic!("routing error: {e}");
+        let route_id = RouteId::next();
+        if let Err(e) = self.matcher.insert(uri, route_id) {
+            panic!("Insert routing rule failed, error: {e}");
         }
+        self.routes
+            .insert(route_id, motore::BoxCloneService::new(route));
+
         self
     }
 }
