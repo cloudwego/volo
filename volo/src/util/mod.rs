4f2e3175d40976a4a986eedcd0f50ff06b584a56
mxsm <ljbmxsm@gmail.com>
2023-11-12 23:38:36 +0800

    [ISSUE #262]Add test case for volo crate util mod

--- volo/src/util/mod.rs
+++ volo/src/util/mod.rs
@@ -57,3 +57,63 @@ impl<B: ?Sized> From<Arc<B>> for Ref<'static, B> {
         Self::Arc(b)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{fmt::Display, sync::Arc};
+
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq, Copy, Clone)]
+    struct TestData;
+
+    impl Display for TestData {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.write_str("TestData")
+        }
+    }
+
+    #[test]
+    fn test_borrowed_ref() {
+        let data = TestData;
+        let borrowed_ref: Ref<'_, TestData> = (&data).into();
+        assert_eq!(*borrowed_ref, data);
+    }
+
+    #[test]
+    fn test_arc_ref() {
+        let data = TestData;
+        let arc_ref: Ref<'static, TestData> = Arc::new(data).into();
+        assert_eq!(*arc_ref, data);
+    }
+
+    #[test]
+    fn test_display_trait() {
+        let data = TestData;
+        let borrowed_ref: Ref<'_, TestData> = (&data).into();
+        assert_eq!(format!("{}", borrowed_ref), format!("{}", data));
+    }
+
+    #[test]
+    fn test_clone_trait() {
+        let data = TestData;
+        let borrowed_ref: Ref<'_, TestData> = (&data).into();
+        let cloned_ref = borrowed_ref.clone();
+        assert_eq!(cloned_ref, borrowed_ref);
+    }
+
+    #[test]
+    fn test_deref_trait() {
+        let data = TestData;
+        let borrowed_ref: Ref<'_, TestData> = (&data).into();
+        assert_eq!(*borrowed_ref, data);
+    }
+
+    #[test]
+    fn test_borrow_trait() {
+        let data = TestData;
+        let borrowed_ref: Ref<'_, TestData> = (&data).into();
+        let borrowed: &TestData = borrowed_ref.borrow();
+        assert_eq!(borrowed, &data);
+    }
+}
