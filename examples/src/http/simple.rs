fd6144dccd1b9590254336512d35883c799a9830
Yu Li <liyu.yukiteru@bytedance.com>
2023-12-07 17:18:29 +0800

    chore(volo-http): update examples for http
    Signed-off-by: Yu Li <liyu.yukiteru@bytedance.com>

--- examples/src/http/simple.rs
+++ examples/src/http/simple.rs
@@ -1,6 +1,7 @@
 use std::{net::SocketAddr, time::Duration};
 
-use serde::Deserialize;
+use faststr::FastStr;
+use serde::{Deserialize, Serialize};
 use volo_http::{
     layer::TimeoutLayer,
     route::{get, post, MethodRouter, Router},
@@ -11,6 +12,45 @@ async fn hello() -> &'static str {
     "hello, world\n"
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+async fn json_get() -> Json<Person> {
+    Json(Person {
+        name: "Foo".to_string(),
+        age: 25,
+        phones: vec!["Bar".to_string(), "114514".to_string()],
+    })
+}
+
+async fn json_post(Json(request): Json<Person>) -> String {
+    let first_phone = request
+        .phones
+        .get(0)
+        .map(|p| p.as_str())
+        .unwrap_or("no number");
+    format!(
+        "{} is {} years old, {}'s first phone number is `{}`\n",
+        request.name, request.age, request.name, first_phone
+    )
+}
+
+async fn test(u: Uri, m: Method, data: FastStr) -> Result<String, (StatusCode, &'static str)> {
+    match m {
+        Method::GET => Err((StatusCode::BAD_REQUEST, "Try POST something\n")),
+        Method::POST => Ok(format!("{m} {u}\n\n{data}\n")),
+        _ => unreachable!(),
+    }
+}
+
+async fn timeout_test() {
+    tokio::time::sleep(Duration::from_secs(10)).await
+}
+
 async fn echo(params: Params) -> Result<Bytes, StatusCode> {
     if let Some(echo) = params.get("echo") {
         return Ok(echo.clone());
@@ -18,42 +58,42 @@ async fn echo(params: Params) -> Result<Bytes, StatusCode> {
     Err(StatusCode::BAD_REQUEST)
 }
 
-#[derive(Deserialize, Debug)]
-struct Person {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
-}
-
-async fn json(Json(request): Json<Person>) {
-    let first_phone = request
-        .phones
-        .get(0)
-        .map(|p| p.as_str())
-        .unwrap_or("no number");
-    println!(
-        "{} is {} years old, {}'s first phone number is {}",
-        request.name, request.age, request.name, first_phone
-    );
-}
-
-async fn test(u: Uri, m: Method) -> Result<&'static str, (StatusCode, &'static str)> {
-    println!("uri:    {u:?}");
-    println!("method: {m:?}");
-    if u.to_string().ends_with("a") {
-        Ok("a") // http://localhost:3000/test?a=a
-    } else {
-        Err((StatusCode::BAD_REQUEST, "b")) // http://localhost:3000/test?a=bb
-    }
-}
-
-async fn timeout_test() {
-    tokio::time::sleep(Duration::from_secs(5)).await
-}
-
 fn timeout_handler(uri: Uri, peer: Address) -> StatusCode {
     tracing::info!("Timeout on `{}`, peer: {}", uri, peer);
     StatusCode::INTERNAL_SERVER_ERROR
+}
+
+fn index_router() -> Router {
+    // curl http://127.0.0.1:8080/
+    Router::new().route("/", get(hello))
+}
+
+fn user_router() -> Router {
+    Router::new()
+        // curl http://localhost:8080/user/json_get
+        .route("/user/json_get", get(json_get))
+        // curl http://localhost:8080/user/json_post \
+        //     -X POST \
+        //     -H "Content-Type: application/json" \
+        //     -d '{"name":"Foo", "age": 25, "phones":["Bar", "114514"]}'
+        .route("/user/json_post", post(json_post))
+}
+
+fn test_router() -> Router {
+    Router::new()
+        // curl http://127.0.0.1:8080/test/extract
+        // curl http://127.0.0.1:8080/test/extract -X POST -d "114514"
+        .route(
+            "/test/extract",
+            MethodRouter::builder().get(test).post(test).build(),
+        )
+        // curl http://127.0.0.1:8080/test/timeout
+        .route(
+            "/test/timeout",
+            get(timeout_test).layer(TimeoutLayer::new(Duration::from_secs(1), timeout_handler)),
+        )
+        // curl -v http://127.0.0.1:8080/test/param/114514
+        .route("/test/param/:echo", get(echo))
 }
 
 #[tokio::main(flavor = "multi_thread")]
@@ -65,22 +105,14 @@ async fn main() {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let app = Router::new()
-        .route(
-            "/",
-            get(hello).layer(TimeoutLayer::new(Duration::from_secs(1), || {
-                StatusCode::INTERNAL_SERVER_ERROR
-            })),
-        )
-        .route("/:echo", get(echo))
-        .route("/user", post(json))
-        .route(
-            "/test",
-            MethodRouter::builder().get(test).post(test).build(),
-        )
-        .route("/timeout", get(timeout_test))
-        .layer(TimeoutLayer::new(Duration::from_secs(1), timeout_handler));
+        .merge(index_router())
+        .merge(user_router())
+        .merge(test_router())
+        .layer(TimeoutLayer::new(Duration::from_secs(5), || {
+            StatusCode::INTERNAL_SERVER_ERROR
+        }));
 
-    let addr: SocketAddr = "[::]:9091".parse().unwrap();
+    let addr: SocketAddr = "[::]:8080".parse().unwrap();
     let addr = volo::net::Address::from(addr);
 
     println!("Listening on {addr}");
