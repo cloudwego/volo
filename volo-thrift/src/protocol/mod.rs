c0a12cf15e80806d07a6cfe02bb8b4a0d73f06a1
Pure White <wudi.daniel@bytedance.com>
2023-12-20 16:34:03 +0800

    feat(volo-thrift): use fifo instead of filo for pool

--- volo-thrift/src/protocol/mod.rs
+++ volo-thrift/src/protocol/mod.rs
@@ -1,5 +1,3 @@
-pub use binary::TBinaryProtocol;
 pub use pilota::thrift::{
-    binary, TFieldIdentifier, TInputProtocol, TLengthProtocol, TListIdentifier, TMapIdentifier,
-    TMessageIdentifier, TMessageType, TOutputProtocol, TSetIdentifier, TStructIdentifier, TType,
+    TInputProtocol, TLengthProtocol, TMessageIdentifier, TMessageType, TOutputProtocol,
 };
