178ef5fd824760f72178af9c3662e9dd651cb4a6
Yukiteru Li <liyu.yukiteru@bytedance.com>
2024-04-15 15:29:15 +0800

    chore(ci): update ci and selftest for checking cli templates (#419)
    Signed-off-by: Yu Li <liyu.yukiteru@bytedance.com>

--- scripts/selftest.sh
+++ scripts/selftest.sh
@@ -5,24 +5,78 @@ set -o nounset
 set -o pipefail
 
 echo_and_run() {
-    echo "Running \`$@\`..."
-    "$@"
+	echo "Running \`$@\`..."
+	"$@"
 }
 
-echo_and_run cargo fmt -- --check
+quiet() {
+	"$@" > /dev/null 2>&1
+}
 
-echo_and_run cargo clippy -p volo-thrift --no-default-features -- --deny warnings
-echo_and_run cargo clippy -p volo-thrift --no-default-features --features multiplex -- --deny warnings
-echo_and_run cargo clippy -p volo-thrift --no-default-features --features unsafe-codec -- --deny warnings
-echo_and_run cargo clippy -p volo-grpc --no-default-features -- --deny warnings
-echo_and_run cargo clippy -p volo-grpc --no-default-features --features rustls -- --deny warnings
-echo_and_run cargo clippy -p volo-grpc --no-default-features --features native-tls -- --deny warnings
-echo_and_run cargo clippy -p volo-grpc --no-default-features --features native-tls-vendored -- --deny warnings
-echo_and_run cargo clippy -p volo-http --no-default-features -- --deny warnings
-echo_and_run cargo clippy -p volo-http --no-default-features --features default_client -- --deny warnings
-echo_and_run cargo clippy -p volo-http --no-default-features --features default_server -- --deny warnings
-echo_and_run cargo clippy -p volo-http --no-default-features --features client,server,serde_json -- --deny warnings
-echo_and_run cargo clippy -p volo-http --no-default-features --features full -- --deny warnings
-echo_and_run cargo clippy -- --deny warnings
+test_in_empty_dir() {
+	local tmp_dir="$(mktemp --tmpdir --directory volo_cli.XXXX)"
+	quiet pushd "${tmp_dir}"
 
-echo_and_run cargo test
+	echo_and_run "$@"
+
+	quiet popd
+	rm -rf "${tmp_dir}"
+}
+
+fmt_check() {
+	echo_and_run cargo fmt -- --check
+}
+
+clippy_check() {
+	# check and clippy for all crates and common features
+	echo_and_run cargo clippy -p volo-thrift --no-default-features -- --deny warnings
+	echo_and_run cargo clippy -p volo-thrift --no-default-features --features multiplex -- --deny warnings
+	echo_and_run cargo clippy -p volo-thrift --no-default-features --features unsafe-codec -- --deny warnings
+	echo_and_run cargo clippy -p volo-grpc --no-default-features -- --deny warnings
+	echo_and_run cargo clippy -p volo-grpc --no-default-features --features rustls -- --deny warnings
+	echo_and_run cargo clippy -p volo-grpc --no-default-features --features native-tls -- --deny warnings
+	echo_and_run cargo clippy -p volo-grpc --no-default-features --features native-tls-vendored -- --deny warnings
+	echo_and_run cargo clippy -p volo-http --no-default-features -- --deny warnings
+	echo_and_run cargo clippy -p volo-http --no-default-features --features default_client -- --deny warnings
+	echo_and_run cargo clippy -p volo-http --no-default-features --features default_server -- --deny warnings
+	echo_and_run cargo clippy -p volo-http --no-default-features --features client,server,serde_json -- --deny warnings
+	echo_and_run cargo clippy -p volo-http --no-default-features --features full -- --deny warnings
+	echo_and_run cargo clippy -- --deny warnings
+}
+
+unit_test() {
+	echo_and_run cargo test
+}
+
+volo_cli_test() {
+	cargo build -p volo-cli -j `nproc`
+	local volo_cli="$PWD/target/debug/volo"
+	local thrift_idl="$PWD/examples/thrift_idl/echo.thrift"
+	local pb_idl="$PWD/examples/proto/echo.proto"
+	local pb_idl_filename="$(basename "${pb_idl}")"
+
+	# thrift
+	test_in_empty_dir bash -c "\
+\"${volo_cli}\" init thrift-test \"${thrift_idl}\" && \
+cargo build -j \`nproc\`"
+
+	# grpc
+	test_in_empty_dir bash -c "\
+mkdir idl && cp \"${pb_idl}\" idl && \
+\"${volo_cli}\" init --includes idl grpc-test \"idl/${pb_idl_filename}\" && \
+cargo build -j \`nproc\`"
+
+	# http
+	test_in_empty_dir bash -c "\
+\"${volo_cli}\" http init http-test && \
+cargo build -j \`nproc\`"
+}
+
+main() {
+	fmt_check
+	clippy_check
+	unit_test
+	volo_cli_test
+}
+
+main
