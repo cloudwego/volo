2553078b2d81673d1548d10187da317c67180995
Yu Li <liyu.yukiteru@bytedance.com>
2024-09-24 18:49:53 +0800

    chore(volo-http): refactor websocket
    Signed-off-by: Yu Li <liyu.yukiteru@bytedance.com>

--- volo-http/src/server/utils/mod.rs
+++ volo-http/src/server/utils/mod.rs
@@ -7,5 +7,3 @@ pub use file_response::FileResponse;
 pub use serve_dir::ServeDir;
 #[cfg(feature = "ws")]
 pub mod ws;
-#[cfg(feature = "ws")]
-pub use self::ws::{Config as WebSocketConfig, Message, WebSocket, WebSocketUpgrade};
