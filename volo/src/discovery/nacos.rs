ceeeacdd0680217f85b2581455ef664b0f8ef789
intfish123 <intfish@qq.com>
2025-06-17 16:04:26 +0800

    fix: thread resource leak fix: service change info

--- volo/src/discovery/nacos.rs
+++ volo/src/discovery/nacos.rs
@@ -1,13 +1,14 @@
 use crate::context::Endpoint;
-use crate::discovery::{Change, Discover, Instance};
+use crate::discovery::{diff_address, Change, Discover, Instance};
 use crate::loadbalance::error::LoadBalanceError;
 use crate::net;
 use crate::net::Address;
 use anyhow::anyhow;
-use async_broadcast::Receiver;
+use async_broadcast::{Receiver, RecvError};
 use faststr::FastStr;
 use pd_rs_common::svc::nacos::NacosNamingAndConfigData;
 use std::sync::Arc;
+use dashmap::DashMap;
 use tracing::warn;
 
 #[derive(Clone)]
@@ -15,6 +16,7 @@ pub struct NacosDiscover {
     pub nacos_naming_data: Arc<NacosNamingAndConfigData>,
     pub svc_change_sender: async_broadcast::Sender<Change<FastStr>>,
     pub svc_change_receiver: async_broadcast::Receiver<Change<FastStr>>,
+    pub current_svc_instance: Arc<DashMap<FastStr, Vec<Arc<Instance>>>>
 }
 
 impl NacosDiscover {
@@ -59,6 +61,7 @@ impl NacosDiscover {
             nacos_naming_data: inner,
             svc_change_sender: svc_ch_s,
             svc_change_receiver: svc_ch_r,
+            current_svc_instance: Arc::new(DashMap::new()),
         };
 
         let mut r = ret
@@ -67,6 +70,7 @@ impl NacosDiscover {
             .sub_svc_change_receiver
             .clone();
         let s = ret.svc_change_sender.clone();
+        let current_svc_instance = ret.current_svc_instance.clone();
         tokio::spawn(async move {
             loop {
                 match r.recv().await {
@@ -74,28 +78,47 @@ impl NacosDiscover {
                         tracing::debug!("received svc change event: {:?}", recv);
                         let key: FastStr = recv.service_name.clone().into();
                         if let Some(is) = recv.instances.clone() {
-                            let mut _ins_ret = vec![];
+                            let mut new_instance = Vec::with_capacity(is.len());
                             for x in is {
-                                _ins_ret.push(Arc::new(Instance {
-                                    address: net::Address::from(Address::Ip(
-                                        format!("{}:{}", x.ip, x.port).parse().unwrap(),
-                                    )),
-                                    weight: x.weight as u32,
-                                    tags: Default::default(),
-                                }));
+                                match format!("{}:{}", x.ip, x.port).parse() {
+                                    Ok(addr) => new_instance.push(Arc::new(Instance {
+                                        address: net::Address::from(Address::Ip(addr)),
+                                        weight: x.weight as u32,
+                                        tags: Default::default(),
+                                    })),
+                                    Err(e) => tracing::error!(
+                                        "failed to parse instance address: {:?}, err: {}",
+                                        x,
+                                        e
+                                    ),
+                                }
                             }
 
-                            let ch = Change {
-                                key: key,
-                                all: _ins_ret,
-                                added: Default::default(),
-                                updated: Default::default(),
-                                removed: Default::default(),
-                            };
+                            let mut pre_svc_instance = vec![];
+                            match current_svc_instance.get(key.as_str()) {
+                                Some(instance) => {
+                                    pre_svc_instance.extend(instance.value().iter().cloned());
+                                }
+                                None => {}
+                            }
+
+                            let (ch, is_change) =
+                                diff_address(key.clone(), pre_svc_instance, new_instance.clone());
+                            if is_change {
+                                current_svc_instance.insert(key, new_instance);
+                            }
+
+                            // always broadcast
                             let _ = s.try_broadcast(ch);
                         }
                     }
-                    Err(err) => warn!("nacos discovering subscription error: {:?}", err),
+                    Err(err) => {
+                        match err {
+                            // if the channel is closed, break
+                            RecvError::Closed => break,
+                            _ => warn!("nacos discovering subscription error: {:?}", err),
+                        }
+                    }
                 }
             }
         });
@@ -112,23 +135,30 @@ impl Discover for NacosDiscover {
         &'s self,
         endpoint: &'s Endpoint,
     ) -> Result<Vec<Arc<Instance>>, Self::Error> {
+        let key = endpoint.service_name.clone();
+        
         let inst_list = self
             .nacos_naming_data
             .event_listener
             .sub_svc_map
             .get(endpoint.service_name.as_str());
         if let Some(inst_list) = inst_list {
-            let mut _ins_ret = vec![];
+            let mut new_instance = Vec::with_capacity(inst_list.len());
             for x in inst_list.iter() {
-                _ins_ret.push(Arc::new(Instance {
-                    address: net::Address::from(Address::Ip(
-                        format!("{}:{}", x.ip, x.port).parse().unwrap(),
-                    )),
-                    weight: x.weight as u32,
-                    tags: Default::default(),
-                }));
+                match format!("{}:{}", x.ip, x.port).parse() {
+                    Ok(addr) => new_instance.push(Arc::new(Instance {
+                        address: net::Address::from(Address::Ip(addr)),
+                        weight: x.weight as u32,
+                        tags: Default::default(),
+                    })),
+                    Err(e) => {
+                        tracing::error!("failed to parse instance address: {:?}, err: {}", x, e)
+                    }
+                }
             }
-            Ok(_ins_ret)
+
+            self.current_svc_instance.insert(key, new_instance.clone());
+            Ok(new_instance)
         } else {
             let ee = anyhow!("no instances for {}", endpoint.service_name.to_string()).into();
             Err(LoadBalanceError::Discover(ee))
@@ -146,12 +176,12 @@ impl Discover for NacosDiscover {
 
 #[cfg(test)]
 mod tests {
-    use crate::context::Endpoint;
-    use crate::discovery::nacos::NacosDiscover;
-    use crate::discovery::{Discover, Instance};
-    use crate::net::Address;
-    use pd_rs_common::svc::nacos::NacosNamingAndConfigData;
-    use std::sync::Arc;
+    // use crate::context::Endpoint;
+    // use crate::discovery::nacos::NacosDiscover;
+    // use crate::discovery::{Discover, Instance};
+    // use crate::net::Address;
+    // use pd_rs_common::svc::nacos::NacosNamingAndConfigData;
+    // use std::sync::Arc;
 
     #[tokio::test]
     async fn test_nacos_discover() {
