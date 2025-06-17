d7c70547c510a36c045ceb5677122f8ad5e99376
intfish123 <intfish@qq.com>
2025-06-17 18:59:17 +0800

    fix: code lint

--- volo-grpc/src/layer/loadbalance/mod.rs
+++ volo-grpc/src/layer/loadbalance/mod.rs
@@ -63,11 +63,9 @@ where
                 loop {
                     match channel.recv().await {
                         Ok(recv) => lb.rebalance(recv),
-                        Err(err) => {
-                            match err {
-                                RecvError::Closed => break,
-                                _ => warn!("[VOLO] discovering subscription error {:?}", err)
-                            }
+                        Err(err) => match err {
+                            RecvError::Closed => break,
+                            _ => warn!("[VOLO] discovering subscription error {:?}", err),
                         },
                     }
                 }
