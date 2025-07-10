aa4d6f8e10af095382dbe13ff67e28f6d5bb75a7
Pure White <wudi.daniel@bytedance.com>
2025-07-10 22:13:40 +0800

    fix(volo-thrift): filter server remote closed log

--- volo-thrift/src/transport/mod.rs
+++ volo-thrift/src/transport/mod.rs
@@ -3,5 +3,12 @@ pub(crate) mod incoming;
 pub mod multiplex;
 pub mod pingpong;
 pub mod pool;
-
+use pilota::thrift::ThriftException;
 pub use pool::Config;
+
+fn server_should_log(e: &ThriftException) -> bool {
+    !matches!(e, ThriftException::Transport(te)
+        if volo::util::server_remote_error::is_remote_closed_error(te.io_error())
+            && !volo::util::server_remote_error::remote_closed_error_log_enabled()
+    )
+}
